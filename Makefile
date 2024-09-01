75e6fc4b473b549f38402766010ca1ef49e8879e
綦文举 <qiwenju@bytedance.com>
2024-09-01 14:44:48 +0800

    使用一个proto文件，将生成代码的绝对路径导入，改为相对路径导入

--- Makefile
+++ Makefile
@@ -1,38 +1,32 @@
+.PHONY: all service link-gen clean
 # 定义.proto文件的路径
 PROTO_DIR = protos
 
 # 定义生成的Python文件的输出路径
 GEN_DIR = gen
-CLIENT_OUT_DIR = $(GEN_DIR)/client
-MASTER_OUT_DIR = $(GEN_DIR)/master
-WORKER_OUT_DIR = $(GEN_DIR)/worker
+SERVICE_OUT_DIR = $(GEN_DIR)
 
 # 定义.proto文件
-CLIENT_PROTO = $(PROTO_DIR)/client.proto
-MASTER_PROTO = $(PROTO_DIR)/master.proto
-WORKER_PROTO = $(PROTO_DIR)/worker.proto
+SERVICE_PROTO = $(PROTO_DIR)/service.proto
 
 # 定义protoc命令
 PROTOC_CMD = python3 -m grpc_tools.protoc -I$(PROTO_DIR)
 
 # 默认目标
-all: client master worker
+service: $(SERVICE_PROTO) clean
+	@mkdir -p $(SERVICE_OUT_DIR)
+	@$(PROTOC_CMD) --python_out=$(SERVICE_OUT_DIR) --grpc_python_out=$(SERVICE_OUT_DIR) --pyi_out=$(SERVICE_OUT_DIR) $(SERVICE_PROTO)
+	@sed -i '' 's/import \(.*\) as \(.*\)/from . import \1 as \2/g' $(SERVICE_OUT_DIR)/service_pb2_grpc.py
 
-# 生成client的中间代码
-client: $(CLIENT_PROTO)
-	mkdir -p $(CLIENT_OUT_DIR)
-	$(PROTOC_CMD) --python_out=$(CLIENT_OUT_DIR) --grpc_python_out=$(CLIENT_OUT_DIR) --pyi_out=$(CLIENT_OUT_DIR) $(CLIENT_PROTO)
+link-gen: service
+	@ln -sfn $(CURDIR)/gen $(CURDIR)/master/gen
+	@ln -sfn $(CURDIR)/gen $(CURDIR)/client/gen
+	@ln -sfn $(CURDIR)/gen $(CURDIR)/worker/gen
 
-# 生成master的中间代码
-master: $(MASTER_PROTO)
-	mkdir -p $(MASTER_OUT_DIR)
-	$(PROTOC_CMD) --python_out=$(MASTER_OUT_DIR) --grpc_python_out=$(MASTER_OUT_DIR) --pyi_out=$(MASTER_OUT_DIR) $(MASTER_PROTO)
+all: link-gen
 
-# 生成worker的中间代码
-worker: $(WORKER_PROTO)
-	mkdir -p $(WORKER_OUT_DIR)
-	$(PROTOC_CMD) --python_out=$(WORKER_OUT_DIR) --grpc_python_out=$(WORKER_OUT_DIR) --pyi_out=$(WORKER_OUT_DIR) $(WORKER_PROTO)
-
-# 清理生成的文件
 clean:
-	rm -rf $(GEN_DIR)
+	@rm -rf $(GEN_DIR)/*
+	@rm -rf $(CURDIR)/master/gen
+	@rm -rf $(CURDIR)/client/gen
+	@rm -rf $(CURDIR)/worker/gen
--- gen/service_pb2.pyi
+++ gen/service_pb2.pyi
@@ -8,10 +8,10 @@ DESCRIPTOR: _descriptor.FileDescriptor
 
 class TaskType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     __slots__ = ()
-    M: _ClassVar[TaskType]
-    R: _ClassVar[TaskType]
-M: TaskType
-R: TaskType
+    MAP: _ClassVar[TaskType]
+    REDUCE: _ClassVar[TaskType]
+MAP: TaskType
+REDUCE: TaskType
 
 class ServerAddress(_message.Message):
     __slots__ = ("ip", "port")
@@ -66,18 +66,18 @@ class ReportTaskResponse(_message.Message):
     def __init__(self, success: bool = ...) -> None: ...
 
 class UploadTaskRequest(_message.Message):
-    __slots__ = ("mNum", "rNum", "serverAddress", "mapReduceFuncPath", "filePaths")
-    MNUM_FIELD_NUMBER: _ClassVar[int]
-    RNUM_FIELD_NUMBER: _ClassVar[int]
+    __slots__ = ("mapNum", "reduceNum", "serverAddress", "mapReduceFuncPath", "filePaths")
+    MAPNUM_FIELD_NUMBER: _ClassVar[int]
+    REDUCENUM_FIELD_NUMBER: _ClassVar[int]
     SERVERADDRESS_FIELD_NUMBER: _ClassVar[int]
     MAPREDUCEFUNCPATH_FIELD_NUMBER: _ClassVar[int]
     FILEPATHS_FIELD_NUMBER: _ClassVar[int]
-    mNum: int
-    rNum: int
+    mapNum: int
+    reduceNum: int
     serverAddress: ServerAddress
     mapReduceFuncPath: FilePath
     filePaths: _containers.RepeatedCompositeFieldContainer[FilePath]
-    def __init__(self, mNum: _Optional[int] = ..., rNum: _Optional[int] = ..., serverAddress: _Optional[_Union[ServerAddress, _Mapping]] = ..., mapReduceFuncPath: _Optional[_Union[FilePath, _Mapping]] = ..., filePaths: _Optional[_Iterable[_Union[FilePath, _Mapping]]] = ...) -> None: ...
+    def __init__(self, mapNum: _Optional[int] = ..., reduceNum: _Optional[int] = ..., serverAddress: _Optional[_Union[ServerAddress, _Mapping]] = ..., mapReduceFuncPath: _Optional[_Union[FilePath, _Mapping]] = ..., filePaths: _Optional[_Iterable[_Union[FilePath, _Mapping]]] = ...) -> None: ...
 
 class UploadTaskResponse(_message.Message):
     __slots__ = ("jobID", "success")
@@ -86,3 +86,43 @@ class UploadTaskResponse(_message.Message):
     jobID: str
     success: bool
     def __init__(self, jobID: _Optional[str] = ..., success: bool = ...) -> None: ...
+
+class ReadFileRequest(_message.Message):
+    __slots__ = ("filePath",)
+    FILEPATH_FIELD_NUMBER: _ClassVar[int]
+    filePath: FilePath
+    def __init__(self, filePath: _Optional[_Union[FilePath, _Mapping]] = ...) -> None: ...
+
+class ReportCompletionRequest(_message.Message):
+    __slots__ = ("jobID", "filePaths")
+    JOBID_FIELD_NUMBER: _ClassVar[int]
+    FILEPATHS_FIELD_NUMBER: _ClassVar[int]
+    jobID: str
+    filePaths: _containers.RepeatedCompositeFieldContainer[FilePath]
+    def __init__(self, jobID: _Optional[str] = ..., filePaths: _Optional[_Iterable[_Union[FilePath, _Mapping]]] = ...) -> None: ...
+
+class ReportCompletionResponse(_message.Message):
+    __slots__ = ("success",)
+    SUCCESS_FIELD_NUMBER: _ClassVar[int]
+    success: bool
+    def __init__(self, success: bool = ...) -> None: ...
+
+class JustDoItRequest(_message.Message):
+    __slots__ = ("jobID", "taskType", "taskID", "mapReduceFuncPath", "filePaths")
+    JOBID_FIELD_NUMBER: _ClassVar[int]
+    TASKTYPE_FIELD_NUMBER: _ClassVar[int]
+    TASKID_FIELD_NUMBER: _ClassVar[int]
+    MAPREDUCEFUNCPATH_FIELD_NUMBER: _ClassVar[int]
+    FILEPATHS_FIELD_NUMBER: _ClassVar[int]
+    jobID: str
+    taskType: TaskType
+    taskID: int
+    mapReduceFuncPath: FilePath
+    filePaths: _containers.RepeatedCompositeFieldContainer[FilePath]
+    def __init__(self, jobID: _Optional[str] = ..., taskType: _Optional[_Union[TaskType, str]] = ..., taskID: _Optional[int] = ..., mapReduceFuncPath: _Optional[_Union[FilePath, _Mapping]] = ..., filePaths: _Optional[_Iterable[_Union[FilePath, _Mapping]]] = ...) -> None: ...
+
+class JustDoItResponse(_message.Message):
+    __slots__ = ("success",)
+    SUCCESS_FIELD_NUMBER: _ClassVar[int]
+    success: bool
+    def __init__(self, success: bool = ...) -> None: ...
